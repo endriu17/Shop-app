a1de8360f3a9e5a0ea69b89f658576ff5980f552
endriu <aolaszewski@gmail.com>
2019-01-10 23:59:36 +0100

    add pagination to productslist

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -2,17 +2,92 @@ import React from "react";
 import "./ProductsList.css";
 import Product from "../Product/Product";
 import data from "../Product/data.json";
+import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
 
-const ProductsList = (props) => (
-  <div className="ProductsList">
-    {data.map((detail, i) => (
-        <Product
-        key={i}
-        {...detail}
-        />
-      )
-    )}
-  </div>
-);
+class ProductsList extends React.Component {
+  constructor() {
+    super();
+
+    this.dataSet = data.map((a, i) => <Product key={i} {...a} />);
+
+    this.pageSize = 6;
+    this.pagesCount = Math.ceil(this.dataSet.length / this.pageSize);
+
+    this.state = {
+      currentPage: 0
+    };
+  }
+
+  handleClick(e, index) {
+    e.preventDefault();
+
+    this.setState({
+      currentPage: index
+    });
+  }
+
+  render() {
+    const { currentPage } = this.state;
+    return (
+      // <div className="ProductsList">
+      //   {data.map((detail, i) => (
+      //     <Product key={i} {...detail} />
+      //   ))}
+      //   <Pager
+      //     length={Math.round(data.length/6)}
+      //     current={this.state.current}
+      //     expansion={3}
+      //     onChange={current => this.setState({ current })}
+      //     showFirstButton={true}
+      //     showLastButton={true}
+      //   />
+      // </div>
+      <React.Fragment>
+        <div className="ProductsList">
+          {this.dataSet
+            .slice(
+              currentPage * this.pageSize,
+              (currentPage + 1) * this.pageSize
+            )
+            .map((data, i) => (
+              <div className="data-slice" key={i}>
+                {data}
+              </div>
+            ))}
+          <div className="pagination-wrapper">
+            <Pagination aria-label="Page navigation example">
+              <PaginationItem disabled={currentPage <= 0}>
+                <PaginationLink
+                  onClick={e => this.handleClick(e, currentPage - 1)}
+                  previous
+                  href="#"
+                />
+              </PaginationItem>
+
+              {[...Array(this.pagesCount)].map((page, i) => (
+                <PaginationItem active={i === currentPage} key={i}>
+                  <PaginationLink
+                    onClick={e => this.handleClick(e, i)}
+                    href="#"
+                  >
+                    {i + 1}
+                  </PaginationLink>
+                </PaginationItem>
+              ))}
+
+              <PaginationItem disabled={currentPage >= this.pagesCount - 1}>
+                <PaginationLink
+                  onClick={e => this.handleClick(e, currentPage + 1)}
+                  next
+                  href="#"
+                />
+              </PaginationItem>
+            </Pagination>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default ProductsList;
