5c5633adab24351437d03ab71d68b3145689f9c3
endriu <aolaszewski@gmail.com>
2019-01-30 21:05:43 +0100

    fix bug with shoppingbag icon

--- src/components/ShoppingBag/ShoppingBag.js
+++ src/components/ShoppingBag/ShoppingBag.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import MediaQuery from "react-responsive";
 
-const ShoppingBag = (props) => {
-
+const ShoppingBag = props => {
   return (
-      <NavLink
-        className="link-nav shoppingBag"
-        to="/bag"
-        activeStyle={{fontWeight: 'bold'}}
-      >
-        <span className="bag-items__icon fas fa-shopping-bag"></span>
-        <span className="bag-items__number">{props.value}</span>
-      </NavLink>
-    );
-  }
+    <NavLink
+      className="link-nav shoppingBag"
+      to="/bag"
+      activeStyle={{ fontWeight: "bold" }}
+    >
+      <MediaQuery query="(min-width: 861px)">
+        <span className="bag-items_icon fas fa-shopping-bag" />
+        <span className="bag-items_number_1">{props.value}</span>
+      </MediaQuery>
+      <MediaQuery query="(max-width: 860px)">
+        <span className="bag-items_icon fas fa-shopping-bag fa-2x" style={{}} />
+        <span className="bag-items_number_2">{props.value}</span>
+      </MediaQuery>
+    </NavLink>
+  );
+};
 
 export default ShoppingBag;
