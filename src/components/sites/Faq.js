0dc86f2ece46811b927ed8e1519947c131ad49a4
endriu <aolaszewski@gmail.com>
2019-02-01 15:05:40 +0100

    add FAQ content and RWD

--- src/components/sites/Faq.js
+++ src/components/sites/Faq.js
@@ -1,10 +1,19 @@
 import React from "react";
+import faq from "./json/faq.json";
 import "./index.css";
 
 const Faq = () => (
   <div className="faq-site">
     <img src="/photos/faq.png" alt={"faq"} />
     <h1>Frequently Asked Questions...</h1>
+    <ul>
+      {faq.map((item, i) => (
+        <li>
+          <h2>{faq[i].question}</h2>
+          <p>{faq[i].answer}</p>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
