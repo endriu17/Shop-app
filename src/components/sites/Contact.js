f9a044f82e3f0e28131e7c13d8083e4b1df031f8
endriu <aolaszewski@gmail.com>
2019-02-04 15:22:53 +0100

    add contact site content

--- src/components/sites/Contact.js
+++ src/components/sites/Contact.js
@@ -1,36 +1,66 @@
 import React from "react";
+import { Col, Button, Form, FormGroup, Label, Input } from "reactstrap";
 import "./index.css";
 
 const Contact = () => (
   <div className="contact-site">
     <img className="contact-icon" src="/photos/contact.png" alt={"contact"} />
-    <h1>You can contact with us...</h1>
-    <form>
-      <label for="fname">First Name</label>
-      <input
-        type="text"
-        id="fname"
-        name="firstname"
-        placeholder="Your name.."
-      />
-
-      <label for="lname">Last Name</label>
-      <input
-        type="text"
-        id="lname"
-        name="lastname"
-        placeholder="Your last name.."
-      />
-
-      <label for="subject">Subject</label>
-      <textarea
-        id="subject"
-        name="subject"
-        placeholder="Write something.."
-      />
-
-      <input type="submit" value="Submit" />
-    </form>
+    <div className="contact-wrapper">
+      <div className="contact-mobile">
+        <span className="contact-label">Need help?</span>
+        <a href="mailto:shop@lighting.com">shop@lighting.com</a>
+        <span className="contact-label">Call us</span>
+        <a href="tel:+11 2233 44556">+11 2233 44556</a>
+        <div className="contact-social">
+          <a href="#" target="_blank">
+            <i className="fab fa-twitter" />
+          </a>
+          <a href="#" target="_blank">
+            <i className="fab fa-instagram" />
+          </a>
+          <a href="#" target="_blank">
+            <i className="fab fa-linkedin" />
+          </a>
+        </div>
+      </div>
+      <div>
+        <span className="contact-label">
+          You can also write to us using the form below:
+        </span>
+        <form id="contact-form">
+          <div>
+            <input
+              id="contact-name"
+              type="text"
+              name="name"
+              placeholder="Name"
+            />
+          </div>
+          <div>
+            <input
+              id="contact-email"
+              type="text"
+              name="email"
+              placeholder="E-mail"
+            />
+          </div>
+          <div>
+            <textarea
+              id="contact-detail"
+              rows="7"
+              name="detail"
+              placeholder="Your message"
+            />
+          </div>
+        </form>
+        <div class="contact-last">
+          <button id="contact-submit" type="submit" name="submit">
+            Send
+          </button>
+        </div>
+        <p>We will answer as soon as possible</p>
+      </div>
+    </div>
   </div>
 );
 
